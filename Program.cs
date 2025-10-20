dad7c09d22b9cf7c9d1cdde720f506260da7fe4a
Minghao Hu (Shanghai Wicresoft Co Ltd) <v-minghu@microsoft.com>
2025-10-20 13:59:46 +0800

    Use another API.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,12 @@
-﻿using System.Runtime.InteropServices;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.Json.Serialization;
-using Microsoft.Win32;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.Processing;
 using Windows.Win32;
-using Windows.Win32.Foundation;
-using Windows.Win32.Graphics.Gdi;
-using Windows.Win32.UI.WindowsAndMessaging;
+using Windows.Win32.UI.Shell;
+
+ChangeWallpaper("");
 
 var configFileContent = File.ReadAllBytes("Config.json");
 var config = JsonSerializer.Deserialize(configFileContent, SourceGenerationContext.Default.Config);
@@ -23,35 +21,10 @@ await StartChangeWallpaperAsync(
 
 static unsafe void ChangeWallpaper(string imagePath)
 {
-    // 设置桌面背景的样式
-    RegistryKey? key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
-    ArgumentNullException.ThrowIfNull(key);
+    var wallpaper = (IDesktopWallpaper)new DesktopWallpaper();
 
-    key.SetValue(@"WallpaperStyle", "10");
-    key.SetValue(@"TileWallpaper", "0");
-
-    // 设置桌面背景
-    var imagePathPtr = Marshal.StringToHGlobalAuto(imagePath);
-    PInvoke.SystemParametersInfo(
-        SYSTEM_PARAMETERS_INFO_ACTION.SPI_SETDESKWALLPAPER,
-        0,
-        imagePathPtr.ToPointer(),
-        SYSTEM_PARAMETERS_INFO_UPDATE_FLAGS.SPIF_UPDATEINIFILE
-            | SYSTEM_PARAMETERS_INFO_UPDATE_FLAGS.SPIF_SENDWININICHANGE
-    );
-    Marshal.FreeHGlobal(imagePathPtr);
-
-    // 刷新桌面
-    var desktop = PInvoke.GetDesktopWindow();
-    PInvoke.RedrawWindow(
-        desktop,
-        (RECT*)0,
-        (HRGN)IntPtr.Zero,
-        REDRAW_WINDOW_FLAGS.RDW_INVALIDATE
-            | REDRAW_WINDOW_FLAGS.RDW_UPDATENOW
-            | REDRAW_WINDOW_FLAGS.RDW_ERASE
-            | REDRAW_WINDOW_FLAGS.RDW_ALLCHILDREN
-    );
+    wallpaper.SetPosition(DESKTOP_WALLPAPER_POSITION.DWPOS_FILL);
+    wallpaper.SetWallpaper(null, imagePath);
 }
 
 static async Task StartChangeWallpaperAsync(string imageFolder, int intervalInSeconds)
@@ -61,7 +34,7 @@ static async Task StartChangeWallpaperAsync(string imageFolder, int intervalInSe
     while (true)
     {
         var images = Directory.GetFiles(imageFolder);
-        images =  [.. images.OrderBy(_ => Random.Shared.Next())];
+        images = [.. images.OrderBy(_ => Random.Shared.Next())];
 
         foreach (var image in images)
         {
